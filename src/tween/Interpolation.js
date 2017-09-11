b716bac9dff5932169b3f1210f2ac4d6baee6f78
Patrick Schroen <ps@ufotechnologies.com>
2017-09-11 16:19:26 -0400

    method definition shorthand

--- src/tween/Interpolation.js
+++ src/tween/Interpolation.js
@@ -110,83 +110,83 @@ class Interpolation {
         };
 
         this.Linear = {
-            None: k => {
+            None(k) {
                 return k;
             }
         };
 
         this.Quad = {
-            In: k => {
+            In(k) {
                 return k * k;
             },
-            Out: k => {
+            Out(k) {
                 return k * (2 - k);
             },
-            InOut: k => {
+            InOut(k) {
                 if ((k *= 2) < 1) return 0.5 * k * k;
                 return -0.5 * (--k * (k - 2) - 1);
             }
         };
 
         this.Cubic = {
-            In: k => {
+            In(k) {
                 return k * k * k;
             },
-            Out: k => {
+            Out(k) {
                 return --k * k * k + 1;
             },
-            InOut: k => {
+            InOut(k) {
                 if ((k *= 2) < 1) return 0.5 * k * k * k;
                 return 0.5 * ((k -= 2) * k * k + 2);
             }
         };
 
         this.Quart = {
-            In: k => {
+            In(k) {
                 return k * k * k * k;
             },
-            Out: k => {
+            Out(k) {
                 return 1 - (--k * k * k * k);
             },
-            InOut: k => {
+            InOut(k) {
                 if ((k *= 2) < 1) return 0.5 * k * k * k * k;
                 return -0.5 * ((k -= 2) * k * k * k - 2);
             }
         };
 
         this.Quint = {
-            In: k => {
+            In(k) {
                 return k * k * k * k * k;
             },
-            Out: k => {
+            Out(k) {
                 return --k * k * k * k * k + 1;
             },
-            InOut: k => {
+            InOut(k) {
                 if ((k *= 2) < 1) return 0.5 * k * k * k * k * k;
                 return 0.5 * ((k -= 2) * k * k * k * k + 2);
             }
         };
 
         this.Sine = {
-            In: k => {
+            In(k) {
                 return 1 - Math.cos(k * Math.PI / 2);
             },
-            Out: k => {
+            Out(k) {
                 return Math.sin(k * Math.PI / 2);
             },
-            InOut: k => {
+            InOut(k) {
                 return 0.5 * (1 - Math.cos(Math.PI * k));
             }
         };
 
         this.Expo = {
-            In: k => {
+            In(k) {
                 return k === 0 ? 0 : Math.pow(1024, k - 1);
             },
-            Out: k => {
+            Out(k) {
                 return k === 1 ? 1 : 1 - Math.pow(2, -10 * k);
             },
-            InOut: k => {
+            InOut(k) {
                 if (k === 0) return 0;
                 if (k === 1) return 1;
                 if ((k *= 2) < 1) return 0.5 * Math.pow(1024, k - 1);
@@ -195,20 +195,20 @@ class Interpolation {
         };
 
         this.Circ = {
-            In: k => {
+            In(k) {
                 return 1 - Math.sqrt(1 - k * k);
             },
-            Out: k => {
+            Out(k) {
                 return Math.sqrt(1 - (--k * k));
             },
-            InOut: k => {
+            InOut(k) {
                 if ((k *= 2) < 1) return -0.5 * (Math.sqrt(1 - k * k) - 1);
                 return 0.5 * (Math.sqrt(1 - (k -= 2) * k) + 1);
             }
         };
 
         this.Elastic = {
-            In: k => {
+            In(k) {
                 let s, a = 0.1, p = 0.4;
                 if (k === 0) return 0;
                 if (k === 1) return 1;
@@ -220,7 +220,7 @@ class Interpolation {
                 }
                 return -(a * Math.pow(2, 10 * (k -= 1)) * Math.sin((k - s) * (2 * Math.PI) / p));
             },
-            Out: k => {
+            Out(k) {
                 let s, a = 0.1, p = 0.4;
                 if (k === 0) return 0;
                 if (k === 1) return 1;
@@ -232,7 +232,7 @@ class Interpolation {
                 }
                 return a * Math.pow(2, -10 * k) * Math.sin((k - s) * (2 * Math.PI) / p) + 1;
             },
-            InOut: k => {
+            InOut(k) {
                 let s, a = 0.1, p = 0.4;
                 if (k === 0) return 0;
                 if (k === 1) return 1;
@@ -248,15 +248,15 @@ class Interpolation {
         };
 
         this.Back = {
-            In: k => {
+            In(k) {
                 let s = 1.70158;
                 return k * k * ((s + 1) * k - s);
             },
-            Out: k => {
+            Out(k) {
                 let s = 1.70158;
                 return --k * k * ((s + 1) * k + s) + 1;
             },
-            InOut: k => {
+            InOut(k) {
                 let s = 1.70158 * 1.525;
                 if ((k *= 2) < 1) return 0.5 * (k * k * ((s + 1) * k - s));
                 return 0.5 * ((k -= 2) * k * ((s + 1) * k + s) + 2);
@@ -264,16 +264,16 @@ class Interpolation {
         };
 
         this.Bounce = {
-            In: k => {
+            In(k) {
                 return 1 - this.Bounce.Out(1 - k);
             },
-            Out: k => {
+            Out(k) {
                 if (k < 1 / 2.75) return 7.5625 * k * k;
                 else if (k < 2 / 2.75) return 7.5625 * (k -= 1.5 / 2.75) * k + 0.75;
                 else if (k < 2.5 / 2.75) return 7.5625 * (k -= 2.25 / 2.75) * k + 0.9375;
                 else return 7.5625 * (k -= 2.625 / 2.75) * k + 0.984375;
             },
-            InOut: k => {
+            InOut(k) {
                 if (k < 0.5) return this.Bounce.In(k * 2) * 0.5;
                 return this.Bounce.Out(k * 2 - 1) * 0.5 + 0.5;
             }

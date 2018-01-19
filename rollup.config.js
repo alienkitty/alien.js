53968fa28cdb0e8760c554c460f143350be61962
Huy Giang <gnhuy91@gmail.com>
2018-01-19 18:24:47 +0700

    add `nosingleton` option

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,49 @@
+import path from 'path';
 import { timestamp, singletons, unexport, babel, uglify } from './src/utils.js';
 
 let pkg = require('./package.json');
 
+function getBuildOutput(format, { babel, uglify, nosingleton } = {}) {
+    const formatSuffix = format === 'umd' ? 'module' : '';
+    const ext = 'js';
+    let output = ['alien', formatSuffix].filter(Boolean).join('.');
+    if (nosingleton) {
+        output = 'nosingleton-' + output;
+    }
+    if (babel) {
+        output = 'es5-' + output;
+    }
+    if (uglify) {
+        output = output + '.min';
+    }
+    return path.join('build', output + '.js');
+}
+
 export default {
     input: 'src/Alien.js',
-    output: [{
-        name: 'Alien',
-        file: process.env.babel ? process.env.uglify ? 'build/es5-alien.module.min.js' : 'build/es5-alien.module.js' : process.env.uglify ? 'build/alien.module.min.js' : 'build/alien.module.js',
-        format: 'umd'
-    }, {
-        file: process.env.babel ? process.env.uglify ? 'build/es5-alien.min.js' : 'build/es5-alien.js' : process.env.uglify ? 'build/alien.min.js' : 'build/alien.js',
-        format: 'es'
-    }],
+    output: [
+        {
+            name: 'Alien',
+            file: getBuildOutput('umd', process.env),
+            format: 'umd'
+        },
+        {
+            file: getBuildOutput('es', process.env),
+            format: 'es'
+        }
+    ],
     plugins: [
-        singletons(),
+        process.env.nosingleton ? {} : singletons(),
         unexport(),
         process.env.babel ? babel() : {},
-        process.env.uglify ? uglify({
-            output: {
-                preamble: `//   _  /._  _  r${pkg.version.split('.')[1]} ${timestamp()}\n//  /_|///_'/ /`
-            }
-        }) : {}
+        process.env.uglify
+            ? uglify({
+                output: {
+                    preamble: `//   _  /._  _  r${
+                        pkg.version.split('.')[1]
+                    } ${timestamp()}\n//  /_|///_'/ /`
+                }
+            })
+            : {}
     ]
 };

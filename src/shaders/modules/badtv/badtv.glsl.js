31033b3d194f6209b2c3aed6de38ebfe04523ed6
Patrick Schroen <ps@ufotechnologies.com>
2021-01-03 22:59:32 -0500

    3D example updates

--- src/shaders/modules/badtv/badtv.glsl.js
+++ src/shaders/modules/badtv/badtv.glsl.js
@@ -1,13 +1,11 @@
-// Based on https://github.com/CODAME/clonicoptics-video-capture
+// Based on https://github.com/felixturner/bad-tv-shader
 
 import simplex2d from '../noise/simplex2d.glsl.js';
 
 export default /* glsl */`
-const float speed = 0.5;
-
 ${simplex2d}
 
-vec4 getBadTV(sampler2D tDiffuse, vec2 uv, float time, float distortion, float distortion2, float rollSpeed) {
+vec4 getBadTV(sampler2D image, vec2 uv, float time, float distortion, float distortion2, float speed, float rollSpeed) {
     vec2 p = uv;
     float ty = time * speed;
     float yt = p.y - ty;
@@ -16,13 +14,12 @@ vec4 getBadTV(sampler2D tDiffuse, vec2 uv, float time, float distortion, float d
     float offset = snoise(vec2(yt * 3.0, 0.0)) * 0.2;
 
     // Boost distortion
-    if (distortion > 0.0) offset = pow(offset * distortion, 3.0) / distortion;
-    else offset = 0.0;
+    offset = offset * distortion * offset * distortion * offset;
 
     // Add fine grain distortion
     offset += snoise(vec2(yt * 50.0, 0.0)) * distortion2 * 0.001;
 
     // Combine distortion on X with roll on Y
-    return texture2D(tDiffuse, vec2(fract(p.x + offset), fract(p.y - time * rollSpeed)));
+    return texture2D(image, vec2(fract(p.x + offset), fract(p.y - time * rollSpeed)));
 }
 `;

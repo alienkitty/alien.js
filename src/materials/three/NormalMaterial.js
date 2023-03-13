38f6b88fd6aa204eb74a8ce7b537aff88546294a
Patrick Schroen <ps@ufotechnologies.com>
2023-03-13 17:22:55 -0400

    Normal material WebGL2 fix

--- src/materials/three/NormalMaterial.js
+++ src/materials/three/NormalMaterial.js
@@ -1,10 +1,11 @@
-import { RawShaderMaterial } from 'three';
+import { GLSL3, RawShaderMaterial } from 'three';
 
 import { vertexShader, fragmentShader } from '../../shaders/NormalShader.js';
 
 export class NormalMaterial extends RawShaderMaterial {
     constructor() {
         super({
+            glslVersion: GLSL3,
             vertexShader,
             fragmentShader
         });

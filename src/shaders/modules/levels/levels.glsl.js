67011523f5e17cca59871cc25a2733986fa7bdbe
Patrick Schroen <ps@ufotechnologies.com>
2024-03-25 12:46:17 -0400

    Readd levels shader module

---
+++ src/shaders/modules/levels/levels.glsl.js
@@ -0,0 +1,15 @@
+// From https://gist.github.com/activetheory/b68c2d639e2bd272b2a7
+
+export default /* glsl */ `
+float levelChannel(float inPixel, float inBlack, float inGamma, float inWhite, float outBlack, float outWhite) {
+    return (pow(((inPixel * 255.0) - inBlack) / (inWhite - inBlack), inGamma) * (outWhite - outBlack) + outBlack) / 255.0;
+}
+
+vec3 levels(vec3 inPixel, float inBlack, float inGamma, float inWhite, float outBlack, float outWhite) {
+    vec3 o = vec3(1.0);
+    o.r = levelChannel(inPixel.r, inBlack, inGamma, inWhite, outBlack, outWhite);
+    o.g = levelChannel(inPixel.g, inBlack, inGamma, inWhite, outBlack, outWhite);
+    o.b = levelChannel(inPixel.b, inBlack, inGamma, inWhite, outBlack, outWhite);
+    return o;
+}
+`;

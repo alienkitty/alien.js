9e0163502a55938d26239aabd1191dd774af5809
Patrick Schroen <ps@ufotechnologies.com>
2024-03-25 07:57:17 -0400

    Map shader module

---
+++ src/shaders/modules/map/map.glsl.js
@@ -0,0 +1,19 @@
+// From https://github.com/msfeldstein/glsl-map
+
+export default /* glsl */ `
+float map(float value, float inMin, float inMax, float outMin, float outMax) {
+  return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+
+vec2 map(vec2 value, vec2 inMin, vec2 inMax, vec2 outMin, vec2 outMax) {
+  return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+
+vec3 map(vec3 value, vec3 inMin, vec3 inMax, vec3 outMin, vec3 outMax) {
+  return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+
+vec4 map(vec4 value, vec4 inMin, vec4 inMax, vec4 outMin, vec4 outMax) {
+  return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+`;

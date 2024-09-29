7bae6115ab25bb50f2292a0dfdb55ce35f94c79c
Patrick Schroen <ps@ufotechnologies.com>
2024-09-28 20:06:38 -0400

    Move shaders to top of material

--- examples/about/src/materials/FluidViewMaterial.js
+++ examples/about/src/materials/FluidViewMaterial.js
@@ -4,6 +4,39 @@ import { WorldController } from '../controllers/world/WorldController.js';
 
 // Based on https://www.shadertoy.com/view/XlsBDf by davidar
 
+const vertexShader = /* glsl */ `
+in vec3 position;
+
+void main() {
+    gl_Position = vec4(position, 1.0);
+}
+`;
+
+const fragmentShader = /* glsl */ `
+precision highp float;
+
+#define TWO_PI 6.28
+
+uniform sampler2D tMap;
+uniform vec2 uResolution;
+
+out vec4 FragColor;
+
+void main() {
+    vec4 c = texture(tMap, gl_FragCoord.xy / uResolution.xy);
+
+    // Velocity
+    FragColor.rgb = 0.6 + 0.6 * cos(6.3 * atan(c.y, c.x) / TWO_PI + vec3(0, 23, 21));
+
+    // Ink
+    FragColor.rgb *= c.w / 5.0;
+
+    // Local fluid density
+    FragColor.rgb += clamp(c.z - 1.0, 0.0, 1.0) / 10.0;
+    FragColor.a = 1.0;
+}
+`;
+
 export class FluidViewMaterial extends RawShaderMaterial {
     constructor() {
         super({
@@ -12,37 +45,8 @@ export class FluidViewMaterial extends RawShaderMaterial {
                 tMap: { value: null },
                 uResolution: WorldController.resolution
             },
-            vertexShader: /* glsl */ `
-                in vec3 position;
-
-                void main() {
-                    gl_Position = vec4(position, 1.0);
-                }
-            `,
-            fragmentShader: /* glsl */ `
-                precision highp float;
-
-                #define TWO_PI 6.28
-
-                uniform sampler2D tMap;
-                uniform vec2 uResolution;
-
-                out vec4 FragColor;
-
-                void main() {
-                    vec4 c = texture(tMap, gl_FragCoord.xy / uResolution.xy);
-
-                    // Velocity
-                    FragColor.rgb = 0.6 + 0.6 * cos(6.3 * atan(c.y, c.x) / TWO_PI + vec3(0, 23, 21));
-
-                    // Ink
-                    FragColor.rgb *= c.w / 5.0;
-
-                    // Local fluid density
-                    FragColor.rgb += clamp(c.z - 1.0, 0.0, 1.0) / 10.0;
-                    FragColor.a = 1.0;
-                }
-            `,
+            vertexShader,
+            fragmentShader,
             blending: NoBlending,
             depthTest: false,
             depthWrite: false

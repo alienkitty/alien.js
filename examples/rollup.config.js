26068de4384fd95ad467406eb23546b9a8fb711d
Patrick Schroen <ps@ufotechnologies.com>
2025-01-27 15:35:37 -0500

    Revert dependencies and OimoPhysics tree-shaking workaround

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -2,6 +2,10 @@ import resolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: '../src/all.three.js',
+  treeshake: {
+    // Needed for OimoPhysics
+    correctVarValueBeforeDeclaration: true
+  },
   output: {
     file: '../build/alien.three.js',
     format: 'es'

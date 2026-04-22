9ab0abe9b573a332ef61cc7907c2d3b4c5aab112
Patrick Schroen <ps@ufotechnologies.com>
2026-04-22 11:44:54 -0400

    Fresnel shader module house cleaning

--- src/shaders/modules/fresnel/fresnel.glsl.js
+++ src/shaders/modules/fresnel/fresnel.glsl.js
@@ -1,5 +1,8 @@
+// Based on https://tympanus.net/codrops/2019/10/29/real-time-multiside-refraction-in-three-steps/ by jespervos
+// Based on https://blog.maximeheckel.com/posts/refraction-dispersion-and-other-shader-light-effects/
+
 export default /* glsl */ `
-float getFresnel(vec3 viewDir, vec3 worldNormal, float power) {
-    return pow(1.0 - abs(dot(viewDir, worldNormal)), power);
+float getFresnel(vec3 eyeVector, vec3 worldNormal, float power) {
+    return pow(1.0 - abs(dot(eyeVector, worldNormal)), power);
 }
 `;
